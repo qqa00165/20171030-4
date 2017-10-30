d665e98590685abcc9e42f2617cf4078b5063f4c
qqa00165 <qqa00165@gmail.com>
2017-10-30 14:25:32 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main {
+
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int a = in.nextInt();
+        if(a<0)
+            a=-a;
+        System.out.println(a);
+
+    }
+}
